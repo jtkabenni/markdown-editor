1fa0943e33719477ab1078deb112d4e2ab2d4fde
Jenny Takahara <jenny.takahara@gmail.com>
2023-12-02 21:39:56 -0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { marked } from "marked";
+import Markdown from "marked-react";
+import ReactMarkdown from "react-markdown";
 function App() {
+  const [input, setInput] = useState("");
+  const [wordCount, setWordCount] = useState(0);
+
+  function countWords() {
+    const trimmedText = input.trim();
+    const words = trimmedText.split(/\s+/);
+    setWordCount(words.length);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputArea">
+        <div className="editorHeader">
+          <h2>Editor</h2>
+          <div className="counts">
+            <p>{input.length} characters</p>
+            <p>{wordCount} words</p>
+          </div>
+        </div>
+
+        <hr></hr>
+        <textarea
+          className="textarea"
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          onInput={countWords}
+        ></textarea>
+      </div>
+      <div className="renderedmarkdown">
+        <h2>Rendered Markdown</h2>
+        <hr></hr>
+        <ReactMarkdown>{input}</ReactMarkdown>
+      </div>
     </div>
   );
 }
